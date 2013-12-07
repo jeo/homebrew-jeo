93db6eb5418fb4a2fa1ab49412c4a2eb0574d7bb
jdeolive <jdeolive@boundlessgeo.com>
2013-12-06 22:07:30 -0700

    updated for 0.2 release

--- Formula/jeo.rb
+++ Formula/jeo.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Jeo < Formula
   homepage 'http://jeo.github.io'
-  url 'http://ares.boundlessgeo.com/jeo/release/0.1/jeo-0.1-cli.zip'
-  sha1 '048b0fde2cc1577853b584e05360878e1944178a'
+  url 'http://ares.boundlessgeo.com/jeo/release/0.2/jeo-0.2-cli.zip'
+  sha1 '668ec7ec5e146d53e44b80b12f152a576ffbc9ee'
 
   def install
     rm Dir['bin/*.{bat,cmd,dll,exe}']
